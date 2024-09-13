a60d4d90ece5549d49c0f135a432f53db5f05d1d
vgrowhouse999 <rlpatenaudeiii@gmail.com>
2024-09-12 21:17:42 -0400

    ?

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-const { Client } = require('pg');
-require('dotenv').config(); // Load environment variables from .env
+const { app, BrowserWindow } = require('electron');
 
-// Use the DATABASE_URL from the environment variables
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Required for cloud-hosted databases
-  }
-});
+let mainWindow;
 
-// Connect to the database
-client.connect();
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
-// Test the connection
-client.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('Database connection error:', err);
-  } else {
-    console.log('Database connected:', res.rows);
-  }
-  client.end();
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 });
